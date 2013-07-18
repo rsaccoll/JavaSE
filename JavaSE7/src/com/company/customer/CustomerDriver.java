37292373e08f4c758a3296f047ab1358e81b6f46
Ramses Almeida <rsaccoll@gmail.com>
2013-07-18 16:24:19 -0300

    Existe um exemplo do livro de certificação Java SE7, assim como exemplo de utilização de Threads

---
+++ JavaSE7/src/com/company/customer/CustomerDriver.java
@@ -0,0 +1,14 @@
+package com.company.customer;
+
+
+
+public class CustomerDriver 
+{
+	public static void main(String []args)
+	{
+		Customer customer;
+		customer = new Customer();
+		customer.setBalance(12506.45f);
+		System.out.println(customer.toString());
+	}
+}
